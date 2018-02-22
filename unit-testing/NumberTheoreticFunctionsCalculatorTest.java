d4d0579a0653847a07ed867ef5774f721caa5f5c
Alonso del Arte <alonso.delarte@gmail.com>
2018-02-22 12:28:49 -0500

    Add files via upload
    Some of these changes were inspired by Kent C. Dodds's post on Medium about making tests less noisy. In particular, the specificity of some of the tests struck me as noisy as I reviewed them.

--- unit-testing/NumberTheoreticFunctionsCalculatorTest.java
+++ unit-testing/NumberTheoreticFunctionsCalculatorTest.java
@@ -26,17 +26,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Tests for a collection of number theoretic functions, including basic primality testing and the Euclidean GCD algorithm.
- * Some of these tests use a finite list of small primes and another finite list of non-prime numbers.
+ * Tests for a collection of number theoretic functions, including basic 
+ * primality testing and the Euclidean GCD algorithm. Some of these tests use a 
+ * finite list of small primes and another finite list of non-prime numbers.
  * @author Alonso del Arte
  */
 public class NumberTheoreticFunctionsCalculatorTest {
     
     /**
-     * setUp() will generate a List of the first few consecutive primes. This 
-     * constant determines how long that list will be.
+     * setUpClass() will generate a List of the first few consecutive primes. 
+     * This constant determines how long that list will be. For example, if it's 
+     * 1000, setUpClass() will generate a list of the primes between 1 and 1000.
+     * It should not be greater than Integer.MAX_VALUE.
      */
-    public static final int PRIME_LIST_SIZE = 1000;
+    public static final int PRIME_LIST_THRESHOLD = 1000;
     
     /**
      * A List of the first few prime numbers, to be used in some of the tests.
@@ -44,34 +47,22 @@ public class NumberTheoreticFunctionsCalculatorTest {
     private static List<Integer> primeList;
     
     /**
-     * The size of primeList
+     * The size of primeList.
      */
     private static int primeListLength;
     
     /**
      * The composite numbers, and 1, listed in <a href="http://oeis.org/A018252">Sloane's A018252</a>.
-     * A lot more of these are listed in the B-file, but these should be sufficient to test isPrime().
+     * A lot more of these are listed in the B-file (see link), but these should 
+     * be sufficient to test isPrime().
      */
     public static final int[] SLOANES_OEIS_A018252 = {1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57, 58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75, 76, 77, 78, 80, 81, 82, 84, 85, 86, 87, 88};
-    
-    /**
-     * A List to be populated with the factors of 44100.
-     * The prime factorization of 44100 is: 2^2 * 3^2 * 5^2 * 7^2.
-     */
-    public static List<Integer> factors44100;
-    
-    /**
-     * A List to be populated with the factors of 86436000, a multiple of 44100.
-     * The prime factorization of 86436000 is: 2^5 * 3^2 * 5^3 * 7^4.
-     */
-    public static List<Integer> factors86436000;
     
     public NumberTheoreticFunctionsCalculatorTest() {
     }
     
     /**
-     * Sets up a List of the first few consecutive primes, a List of the factors 
-     * of 44100 and another List with the factors of 86436000. The static final 
+     * Sets up a List of the first few consecutive primes. The static final 
      * fields provide most of everything else needed for the tests.
      */
     @BeforeClass
@@ -79,10 +70,10 @@ public class NumberTheoreticFunctionsCalculatorTest {
         /* First, to generate a list of the first few consecutive primes, using 
         the sieve of Eratosthenes. */
         int threshold, halfThreshold;
-        if (PRIME_LIST_SIZE < 0) {
-            threshold = (-1) * PRIME_LIST_SIZE;
+        if (PRIME_LIST_THRESHOLD < 0) {
+            threshold = (-1) * PRIME_LIST_THRESHOLD;
         } else {
-            threshold = PRIME_LIST_SIZE;
+            threshold = PRIME_LIST_THRESHOLD;
         }
         if (threshold % 2 == 1) {
             halfThreshold = (threshold + 1)/2;
@@ -112,32 +103,6 @@ public class NumberTheoreticFunctionsCalculatorTest {
         primeListLength = primeList.size();
         System.out.println("setUpClass() has generated a list of the first " + primeListLength + " consecutive primes.");
         System.out.println("prime(" + primeListLength + ") = " + primeList.get(primeListLength - 1));
-        // 44100 = 2^2 * 3^2 * 5^2 * 7^2
-        factors44100 = new ArrayList<>();
-        factors44100.add(2);
-        factors44100.add(2); // 4
-        factors44100.add(3);
-        factors44100.add(3); // 9
-        factors44100.add(5);
-        factors44100.add(5); // 25
-        factors44100.add(7);
-        factors44100.add(7); // 49
-        // 86436000 = 2^5 * 3^2 * 5^3 * 7^4
-        factors86436000 = new ArrayList<>();
-        factors86436000.add(2);
-        factors86436000.add(2);
-        factors86436000.add(2);
-        factors86436000.add(2);
-        factors86436000.add(2); // 32
-        factors86436000.add(3);
-        factors86436000.add(3); // 9
-        factors86436000.add(5);
-        factors86436000.add(5);
-        factors86436000.add(5); // 125
-        factors86436000.add(7);
-        factors86436000.add(7);
-        factors86436000.add(7);
-        factors86436000.add(7); // 2401
     }
     
     @AfterClass
@@ -154,34 +119,73 @@ public class NumberTheoreticFunctionsCalculatorTest {
 
     /**
      * Test of primeFactors method, of class NumberTheoreticFunctionsCalculator.
+     * This test uses squares of primorials (4, 36, 900, 44100, etc.) and 
+     * certain divisors of those numbers. This test also checks the 
+     * factorization of the additive inverses of those numbers (e.g., -44100).
+     * The expectation is that if -n is a negative number, its factorization 
+     * will be the same as that of n but with a single -1 at the beginning. This
+     * test does not set out any expectations for the factorization of 0.
      */
     @Test
     public void testPrimeFactors() {
         System.out.println("primeFactors");
-        int num = 32;
+        int num = 1;
+        int primeIndex = 0;
+        List<Integer> result = new ArrayList<>();
         List<Integer> expResult = new ArrayList<>();
-        expResult.add(2);
-        expResult.add(2); // 4
-        expResult.add(2); // 8
-        expResult.add(2); // 16
-        expResult.add(2); // 32
-        List<Integer> result = NumberTheoreticFunctionsCalculator.primeFactors(num);
-        assertEquals(expResult, result);
-        num = 44100;
-        expResult = factors44100;
-        result = NumberTheoreticFunctionsCalculator.primeFactors(num);
-        assertEquals(expResult, result);
-        num = 86436000;
-        expResult = factors86436000;
-        result = NumberTheoreticFunctionsCalculator.primeFactors(num);
-        assertEquals(expResult, result);
+        boolean withinRange = true;
+        int lastNumTested = 1;
+        while (withinRange) {
+            num *= primeList.get(primeIndex);
+            expResult.add(primeList.get(primeIndex));
+            /* Check the number has not overflown the integer data type and that
+            we're not going beyond our finite list of primes */
+            withinRange = (num > 0 && num < Integer.MAX_VALUE) && (primeIndex < primeListLength - 1);
+            if (withinRange) {
+                result = NumberTheoreticFunctionsCalculator.primeFactors(num);
+                assertEquals(result, expResult);
+                num *= -1; // Make num negative
+                expResult.add(0, -1);
+                result = NumberTheoreticFunctionsCalculator.primeFactors(num);
+                assertEquals(result, expResult);
+                lastNumTested = num;
+                num *= -1; // And back to positive
+                expResult.remove(0);
+                // Now to test factorization on the square of a primorial
+                num *= primeList.get(primeIndex);
+                expResult.add(primeList.get(primeIndex));
+                // Checking for integer data type overflow only at this point
+                withinRange = (num > 0 && num < Integer.MAX_VALUE);
+                if (withinRange) {
+                    result = NumberTheoreticFunctionsCalculator.primeFactors(num);
+                    assertEquals(result, expResult);
+                    num *= -1; // Make num negative
+                    expResult.add(0, -1);
+                    result = NumberTheoreticFunctionsCalculator.primeFactors(num);
+                    assertEquals(result, expResult);
+                    lastNumTested = num;
+                    num *= -1; // And back to positive
+                    expResult.remove(0);
+                }
+            }
+            primeIndex++;
+        }
+        System.out.print("Last number tested was " + lastNumTested + ", which has this factorization: " + result.get(0));
+        for (int i = 1; i < result.size(); i++) {
+            System.out.print(" \u00D7 ");
+            System.out.print(result.get(i));
+        }
+        System.out.println(" ");
     }
 
     /**
-     * Test of isPrime method, of class NumberTheoreticFunctionsCalculator.
-     * The numbers listed in Sloane's A000040, as well as those same numbers multiplied by -1, should all be identified as prime.
-     * Likewise, the numbers listed in Sloane's A018252, as well as those same numbers multiplied by -1, should all be identified as not prime.
-     * As for 0, I'm not sure; if you like you can uncomment the line for it and perhaps change assertFalse to assertTrue.
+     * Test of isPrime method, of class NumberTheoreticFunctionsCalculator. The 
+     * numbers listed in Sloane's A000040, as well as those same numbers 
+     * multiplied by -1, should all be identified as prime. Likewise, the 
+     * numbers listed in Sloane's A018252, as well as those same numbers 
+     * multiplied by -1, should all be identified as not prime. As for 0, I'm 
+     * not sure; if you like you can uncomment the line for it and perhaps 
+     * change assertFalse to assertTrue.
      */
     @Test
     public void testIsPrime() {
@@ -223,10 +227,7 @@ public class NumberTheoreticFunctionsCalculatorTest {
     @Test
     public void testIsSquareFree() {
         System.out.println("isSquareFree");
-        int num = 1024;
-        boolean expResult = false;
-        boolean result = NumberTheoreticFunctionsCalculator.isSquareFree(num);
-        assertEquals(expResult, result);
+        int num;
         for (int i = 0; i < primeListLength - 1; i++) {
             num = primeList.get(i) * primeList.get(i + 1); // A squarefree semiprime, pq
             assertTrue(NumberTheoreticFunctionsCalculator.isSquareFree(num));
@@ -237,61 +238,63 @@ public class NumberTheoreticFunctionsCalculatorTest {
 
     /**
      * Test of moebiusMu method, of class NumberTheoreticFunctionsCalculator.
-     * I expect that mu(-n) = mu(n), so this test checks for that.
-     * If you wish to limit the test to positive integers, you can just remove some of the assertions.
+     * I expect that mu(-n) = mu(n), so this test checks for that. If you wish 
+     * to limit the test to positive integers, you can just remove some of the 
+     * assertions.
      */
     @Test
     public void testMoebiusMu() {
         System.out.println("moebiusMu");
-        int num = 1074;
         byte expResult = -1;
-        byte result = NumberTheoreticFunctionsCalculator.moebiusMu(num);
-        assertEquals(expResult, result);
+        byte result;
         // The primes p should all have mu(p) = -1
         for (int i = 0; i < primeListLength; i++) {
-            assertEquals(expResult, NumberTheoreticFunctionsCalculator.moebiusMu(primeList.get(i)));
-            assertEquals(NumberTheoreticFunctionsCalculator.moebiusMu(primeList.get(i)), NumberTheoreticFunctionsCalculator.moebiusMu(-primeList.get(i)));
+            result = NumberTheoreticFunctionsCalculator.moebiusMu(primeList.get(i));
+            assertEquals(expResult, result);
+            assertEquals(result, NumberTheoreticFunctionsCalculator.moebiusMu(-primeList.get(i)));
         }
         // Now to test mu(n) = 0 with n being a multiple of 4
         expResult = 0;
         for (int j = 0; j < 97; j += 4) {
-            assertEquals(expResult, NumberTheoreticFunctionsCalculator.moebiusMu(j));
-            assertEquals(NumberTheoreticFunctionsCalculator.moebiusMu(j), NumberTheoreticFunctionsCalculator.moebiusMu(-j));
+            result = NumberTheoreticFunctionsCalculator.moebiusMu(j);
+            assertEquals(expResult, result);
+            assertEquals(result, NumberTheoreticFunctionsCalculator.moebiusMu(-j));
         }
         // And lastly, the products of two distinct primes p and q should give mu(pq) = 1
         expResult = 1;
+        int num;
         for (int k = 0; k < primeListLength - 1; k++) {
             num = primeList.get(k) * primeList.get(k + 1);
-            assertEquals(expResult, NumberTheoreticFunctionsCalculator.moebiusMu(num));
-            assertEquals(NumberTheoreticFunctionsCalculator.moebiusMu(num), NumberTheoreticFunctionsCalculator.moebiusMu(-num));
+            result = NumberTheoreticFunctionsCalculator.moebiusMu(num);
+            assertEquals(expResult, result);
+            assertEquals(result, NumberTheoreticFunctionsCalculator.moebiusMu(-num));
         }
     }
 
     /**
      * Test of euclideanGCD method, of class NumberTheoreticFunctionsCalculator.
      * TO DO: Write test for euclideanGCD(IQI, IQI).
-     * At this time, I choose not to test the case gcd(0, 0).
-     * The value of such a test would be philosophical rather than practical.
+     * At this time, I choose not to test the case gcd(0, 0). The value of such 
+     * a test would be philosophical rather than practical.
      */
     @Test
     public void testEuclideanGCD() {
         System.out.println("euclideanGCD");
-        int a = -1925;
-        int b = 44100;
-        int expResult = 175;
-        int result = NumberTheoreticFunctionsCalculator.euclideanGCD(a, b);
-        assertEquals(expResult, result);
-        expResult = 1; // Going to test with consecutive integers, expect the result to be 1 each time
+        int a, b, result;
+        int expResult = 1; /* Going to test with consecutive integers, expect 
+                              the result to be 1 each time */
         for (int i = -30; i < 31; i++) {
             result = NumberTheoreticFunctionsCalculator.euclideanGCD(i, i + 1);
             assertEquals(expResult, result);
         }
-        // Now test with consecutive odd numbers, result should also be 1 each time as well
+        /* Now test with consecutive odd numbers, result should also be 1 each 
+        time as well */
         for (int i = -29; i < 31; i += 2) {
             result = NumberTheoreticFunctionsCalculator.euclideanGCD(i, i + 2);
             assertEquals(expResult, result);
         }
-        expResult = 2; // Now to test with consecutive even integers, result should be 2 each time
+        expResult = 2; /* Now to test with consecutive even integers, result 
+                          should be 2 each time */
         for (int i = -30; i < 31; i += 2) {
             result = NumberTheoreticFunctionsCalculator.euclideanGCD(i, i + 2);
             assertEquals(expResult, result);
@@ -311,7 +314,8 @@ public class NumberTheoreticFunctionsCalculatorTest {
     }
     
     /**
-     * Test of randomNegativeSquarefreeNumber method, of class NumberTheoreticFunctionsCalculator.
+     * Test of randomNegativeSquarefreeNumber method, of class 
+     * NumberTheoreticFunctionsCalculator.
      */
     @Test
     public void testRandomNegativeSquarefreeNumber() {
@@ -329,8 +333,8 @@ public class NumberTheoreticFunctionsCalculatorTest {
             // System.out.print(squaredPrime + ", " + ranNumDiv + ", " + flooredRanNumDiv + "; ");
             assertFalse(ranNumDiv == flooredRanNumDiv);
         }
-        System.out.println();
-        // And lastly, check that it is at least the negated test bound but not more than or equal to 0
+        /* And lastly, check that it is at least the negated test bound but not 
+        more than or equal to 0. */
         assertFalse(potentialNegRanSqFreeNum < (-1) * testBound);
         assertTrue(potentialNegRanSqFreeNum < 0);
     }

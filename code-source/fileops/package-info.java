5325e4a01627c9ff0a25b4d7c7c013ec533bcc9b
Alonso del Arte <alonso.delarte@gmail.com>
2018-09-05 23:34:18 -0400

    Add files via upload

---
+++ code-source/fileops/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2018 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * A helper package to move some file operations out of the 
+ * imaginaryquadraticinteger package.
+ * 
+ * {@link fileops.FileChooserWithOverwriteGuard} extends {@link javax.swing.JFileChooser},
+ * {@link fileops.JPEGFileFilter},
+ * {@link fileops.PNGFileFilter},
+ */
+package fileops;
